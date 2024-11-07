bf54c5d0df80636b611b4d29d49a03ef6839d633
Apurbo <tanvirmahfuz22@gmail.com>
2024-11-07 16:13:49 +0600

    module

--- numpy/assignment 3/apurboMathTest.py
+++
@@ -1,3 +0,0 @@
-from pi_value import pi_value
-n = int(input("Enter the number of decimal places: "))
-print(pi_value(n))
---
+++ numpy/assignment_3/apurboMathTest.py
@@ -0,0 +1,6 @@
+from pi_value import pi_value  
+!wget https://raw.githubusercontent.com/TanvirMahfuz/EDGE-MODULES/main/numpy/assignment%203/apurboMathTest.py
+
+n = int(input("Enter the number of decimal places: "))
+
+print(pi_value(n))
